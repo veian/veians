e7c127aba2fbaf3582ad3a04b27b2fdfefa87066
veian <q.787583174@gmail.com>
2013-12-04 22:53:35 +0800

    ex5

---
+++ ex3.rb
@@ -0,0 +1,13 @@
+puts "I will now count my chickens:"
+
+puts "Hens", 25 + 30 / 6
+
+puts "Roosters", 100 - 25 * 3 % 4
+
+puts "Now I will count the eggs:"
+
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+puts 3 + 2 < 5 - 7
---
+++ ex5.rb
@@ -0,0 +1,8 @@
+my_name = 'Zhu Bin'
+my_age = 25
+my_height = 165 # cm
+my_weight = 115 # kg
+
+puts "Let's talk about %s." % my_name
+puts "He's %dcm tall." % my_height
+puts "He's %dkg heavy." % my_weight
