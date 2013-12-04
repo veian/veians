f3c455c5b842879acf8fbf1abf5b229a38389c06
veian <q.787583174@gmail.com>
2013-12-04 23:58:21 +0800

    ruby 学习的第一天
    http://lrthw.github.io/ex12/
    这个教程
    再接再厉！！！！

---
+++ ex11.rb
@@ -0,0 +1,12 @@
+q = "How"
+q1 = " old are you?"
+print q + q1
+print q + q1
+age = gets.chomp()
+puts "age: #{age}"
+print "How tall are you?"
+height = gets.chomp()
+puts "tall is #{height}"
+print "How much do you weight?"
+weight = gets
+puts "weight is #{weight}"
---
+++ ex9.rb
@@ -0,0 +1,23 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: ", days
+puts "Here are the months: ", months
+
+puts <<PARAGRAPH
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+PARAGRAPH
+
+puts <<a
+kkkkkk
+
+dfkdjfkd
+\t* kdfjdkf
+\t* kdfjdkf
+\t* kdfjdkf
+\t* kdfjdkf
+\t* kdfjdkf
+a
